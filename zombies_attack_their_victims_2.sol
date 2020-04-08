73d9def18759b38057e59a2b8bac295e912e5d07
Marianna <mckd2018@gmail.com>
2020-04-07 22:43:53 -0400

    [add] feeding contract

--- zombies_attack_their_victims_2.sol
+++ zombies_attack_their_victims_2.sol
@@ -36,3 +36,13 @@ contract ZombieFactory {
     }
 
 }
+
+contract ZombieFeeding is ZombieFactory {
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    Zombie storage myZombie = zombies[_zombieId];
+    // start here
+  }
+
+}
